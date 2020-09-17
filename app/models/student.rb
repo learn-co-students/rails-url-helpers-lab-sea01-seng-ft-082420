34e1af2715aa7be08d2b3143fa7b5100e56b749e
JacksonBey <jacksongrowson@gmail.com>
2020-09-17 10:55:26 -0700

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,18 @@
 class Student < ActiveRecord::Base
+  # attr_accessor :active
+  # def initialize(active= false)
+  #   @active = active
+  # end
+
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  # def active
+  #   @active
+  # end
+
+  # def active= (parm)
+  #   @active = parm
+  # end
 end
